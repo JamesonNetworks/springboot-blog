c7a8f3ca4f2a5048bcbcef1528ee142768629372
Brent Jameson <brent@jamesonnetworks.com>
2016-09-19 07:55:57 -0400

    Fixing up some css problems

--- client/www/src/main/util.js
+++ client/www/src/main/util.js
@@ -1,9 +1,3 @@
-import Moment from 'moment';
-import $ from 'jquery';
-
-const TOOLBAR = ".toolbar";
-const ENTRY = ".entry";
-
 export function getBaseUrl() {
     var currentUrl = window.location.href;
     if(currentUrl.indexOf('.dev') > 0) {
