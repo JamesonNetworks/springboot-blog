709142526fb5b0689a1bd90cc8aa45b6e9149a73
Brent Jameson <brent@jamesonnetworks.com>
2017-08-19 12:29:33 -0400

    Add in menu

--- client/www/src/components/header.jsx
+++ client/www/src/components/header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import moment from 'moment';
-import {map, sortBy, filter} from 'lodash';
+import {each, map, sortBy, filter, reverse} from 'lodash';
 
 require("./header.scss");
 
@@ -33,31 +33,52 @@ class Header extends React.Component {
     }
     render() {
         let entries = this.props.entries;
-        let options = map(sortBy(entries, function(entry) { return Date.now() - entry.date }), (entry) => {
+        let years = {};
+        var self = this;
+        each(sortBy(entries, function(entry) { return Date.now() - entry.date }), (entry) => {
             let date = new Date(Number.parseInt(entry.date));
             let prettyDate = moment(date).format("MMM Do YYYY");
-            return ( 
-                <a className="item" href="#" onClick={this.changeHandler} key={entry.date} id={entry.date}>
-                    {entry.title}
-                </a>
-            )
+            let year = prettyDate.substring(prettyDate.length - 4, prettyDate.length);
+            if(_.isUndefined(years[year])) {
+                years[year] = {};
+                years[year].year = year;
+                years[year].entries = [];
+            }
+            years[year].entries.push(entry);
         });
+        let options = reverse(map(years, function(year) {
+            let articles = map(year.entries, function(entry) {
+                let date = new Date(Number.parseInt(entry.date));
+                let prettyDate = moment(date).format("MMM Do YYYY");
+                return (
+                    <a className="item" href="#" onClick={self.changeHandler} key={entry.date} id={entry.date}>
+                        {entry.title} - {prettyDate}
+                    </a>
+                );
+            });
+            return (
+                <div className="item" href="#">
+                    <i className="dropdown icon"></i>
+                    {year.year}
+                    <div className="menu">
+                        {articles}
+                    </div>
+                </div>
+            );
+        }));
         return (
             <div className="ui fixed menu">
                 <div className="ui container">
-                    <a href="#" className="header item">
-                    JamesonNetworks Blog
-                    </a>
                     <a className="ui simple item" onClick={this.onLeftArrowClick}>Previous</a>
+                    <div className="ui simple dropdown item">
+                        Articles
+                        <i className="dropdown icon"></i>
+                        <div className="menu">
+                            {options}
+                        </div>
+                    </div>
                     <div className="right menu">
                         <a className="ui simple item right" onClick={this.onRightArrowClick}>Next</a>
-                        <div className="ui simple dropdown item">
-                            Articles
-                            <i className="dropdown icon"></i>
-                            <div className="menu">
-                                {options}
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
